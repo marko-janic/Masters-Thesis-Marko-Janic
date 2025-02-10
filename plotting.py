e6d0fb5f00ccec458bf35fdec2e69061d0aed159
Marko Janic <marko.janic@hotmail.ch>
2025-02-10 13:55:29 +0100

    Add z threshhold to plotting

--- plotting.py
+++ plotting.py
@@ -11,7 +11,7 @@ matplotlib.use('Agg')  # To avoid error: _tkinter.TclError: no display name and 
 
 
 def save_image_with_bounding_boxes(image_tensor, particle_locations, circle_radius, result_dir,
-                                   file_name, box_color='r'):
+                                   file_name, box_color='r', z_threshold=9999):
     """
     Show the image and draw bounding boxes around the given pixel coordinates.
 
@@ -21,15 +21,21 @@ def save_image_with_bounding_boxes(image_tensor, particle_locations, circle_radi
     :param result_dir: Result directory to save to
     :param file_name: File name of file that is saved
     :param box_color: Border color of drawn boxes
+    :param z_threshold: Threshold for z value to decide if a circle should be plotted
     """
     if not os.path.exists(result_dir):
         raise Exception("The folder you are trying to save to doesn't exist.")
 
     fig, ax = plt.subplots(1)
 
+    # Premute the tensor because imshow wants width x height x channels
+    image_tensor = image_tensor.permute(1, 2, 0)
     ax.imshow(image_tensor, cmap='gray')
 
     for _, row in particle_locations.iterrows():
+        if row['Z'] > z_threshold:
+            continue
+
         box_color = 'b'
         if row['class'] == 'fiducial':
             box_color = 'r'
@@ -38,7 +44,7 @@ def save_image_with_bounding_boxes(image_tensor, particle_locations, circle_radi
 
         x = row['X']
         y = row['Y']
-        rect = patches.Circle((x, y), circle_radius, linewidth=1, edgecolor=box_color, facecolor='none')
-        ax.add_patch(rect)
+        circle = patches.Circle((x, y), circle_radius, linewidth=1, edgecolor=box_color, facecolor='none')
+        ax.add_patch(circle)
 
     plt.savefig(os.path.join(result_dir, file_name))
