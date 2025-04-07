78de642b6cd9b4f8f0da91e75d7fd3a9edf6e0e3
Marko Janic <marko.janic@hotmail.ch>
2025-04-07 21:14:28 +0200

    Update create_dummy_dataset.sh

--- slurm_scripts/create_dummy_dataset.sh
+++ slurm_scripts/create_dummy_dataset.sh
@@ -2,9 +2,9 @@
 #The previous line is mandatory
 
 #SBATCH --job-name=create_dummy_dataset     #Name of your job
-#SBATCH --time=12:00:00       #Maximum allocated time
+#SBATCH --time=6:00:00       #Maximum allocated time
 #SBATCH --qos=gpu6hours       #Selected queue to allocate your job (the gpu before 6hours is important, see comment: https://wiki.biozentrum.unibas.ch/display/scicore/9.+Requesting+GPUs)
-#SBATCH --partition=a100
+#SBATCH --partition=titanx
 #SBATCH --gres=gpu:1          #--gres=gpu:2 for two GPU, etc
 #SBATCH --output=job_results/create_dummy_dataset.o%j   #Path and name to the file for the STDOUT
 #SBATCH --error=job_results/create_dummy_dataset_errors.e%j    #Path and name to the file for the STDERR
