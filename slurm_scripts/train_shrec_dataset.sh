82c11ca073d0931f4a599cd4d98c34cc6205a16e
Marko Janic <marko.janic@hotmail.ch>
2025-05-27 00:50:41 +0200

    Update train_shrec_dataset.sh

--- slurm_scripts/train_shrec_dataset.sh
+++ slurm_scripts/train_shrec_dataset.sh
@@ -13,7 +13,6 @@
 
 module purge
 module load Miniconda3
-conda init
-conda activate masters_thesis_marko_janic
+source activate masters_thesis_marko_janic
 
 python main.py --config run_configs/shrec_dataset_training.json
