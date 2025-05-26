64820ac39de1f3495059fc10b6bf0ec864fc0a61
Marko Janic <marko.janic@hotmail.ch>
2025-05-27 00:38:55 +0200

    Update train_shrec_dataset.sh

--- slurm_scripts/train_shrec_dataset.sh
+++ slurm_scripts/train_shrec_dataset.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=fbp_no_noise_training     #Name of your job
 #SBATCH --time=7-00:00:00    #Maximum allocated time
 #SBATCH --qos=gpu1week       #Selected queue to allocate your job (the gpu before 6hours is important, see comment: https://wiki.biozentrum.unibas.ch/display/scicore/9.+Requesting+GPUs)
-#SBATCH --partition=a100
+#SBATCH --partition=rtx4090
 #SBATCH --gres=gpu:1          #--gres=gpu:2 for two GPU, etc
 #SBATCH --output=job_results/fbp_no_noise_training.o%j   #Path and name to the file for the STDOUT
 #SBATCH --error=job_results/fbp_no_noise_training.e%j    #Path and name to the file for the STDERR
