602cc243edf1584ed01e122ae3884f2231f3f371
Marko Janic <marko.janic@hotmail.ch>
2025-06-08 21:53:59 +0200

    Change partition again

--- slurm_scripts/train_shrec_dataset.sh
+++ slurm_scripts/train_shrec_dataset.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=particle_1U6G_fbp_noise_20db     #Name of your job
 #SBATCH --time=7-00:00:00    #Maximum allocated time
 #SBATCH --qos=gpu1week       #Selected queue to allocate your job (the gpu before 6hours is important, see comment: https://wiki.biozentrum.unibas.ch/display/scicore/9.+Requesting+GPUs)
-#SBATCH --partition=a100-80g
+#SBATCH --partition=a100
 #SBATCH --gres=gpu:1          #--gres=gpu:2 for two GPU, etc
 #SBATCH --output=job_results/particle_1U6G_fbp_noise_20db.o%j   #Path and name to the file for the STDOUT
 #SBATCH --error=job_results/particle_1U6G_fbp_noise_20db.e%j    #Path and name to the file for the STDERR
