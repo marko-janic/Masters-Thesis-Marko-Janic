ed99d8244f86f3cdb1f093c437cbc1844eda01f7
Marko Janic <marko.janic@hotmail.ch>
2025-06-09 23:26:40 +0200

    Prepare scripts for experiments

--- slurm_scripts/train_shrec_dataset.sh
+++ slurm_scripts/train_shrec_dataset.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #The previous line is mandatory
 
-#SBATCH --job-name=multiple_shrec_volumes_smaller_model_128_64_32     #Name of your job
+#SBATCH --job-name=multiple_shrec_volumes_model_64_32_16     #Name of your job
 #SBATCH --time=7-00:00:00    #Maximum allocated time
 #SBATCH --qos=gpu1week       #Selected queue to allocate your job (the gpu before 6hours is important, see comment: https://wiki.biozentrum.unibas.ch/display/scicore/9.+Requesting+GPUs)
 #SBATCH --partition=titan
 #SBATCH --gres=gpu:1          #--gres=gpu:2 for two GPU, etc
-#SBATCH --output=job_results/multiple_shrec_volumes_smaller_model_128_64_32.o%j   #Path and name to the file for the STDOUT
-#SBATCH --error=job_results/multiple_shrec_volumes_smaller_model_128_64_32.e%j    #Path and name to the file for the STDERR
+#SBATCH --output=job_results/multiple_shrec_volumes_model_64_32_16.o%j   #Path and name to the file for the STDOUT
+#SBATCH --error=job_results/multiple_shrec_volumes_model_64_32_16.e%j    #Path and name to the file for the STDERR
 #SBATCH --cpus-per-task=8     #Number of cores to reserve
 #SBATCH --mem-per-cpu=8G     #Amount of RAM/core to reserve
 
