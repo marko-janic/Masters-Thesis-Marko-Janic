a0465d0cee747ab01414235707bdd865f7815081
Marko Janic <marko.janic@hotmail.ch>
2025-06-10 01:01:54 +0200

    Adjust partition

--- slurm_scripts/train_shrec_dataset.sh
+++ slurm_scripts/train_shrec_dataset.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=model_with_dropout2d_layers_prob_0_2     #Name of your job
 #SBATCH --time=7-00:00:00    #Maximum allocated time
 #SBATCH --qos=gpu1week       #Selected queue to allocate your job (the gpu before 6hours is important, see comment: https://wiki.biozentrum.unibas.ch/display/scicore/9.+Requesting+GPUs)
-#SBATCH --partition=rtx4090
+#SBATCH --partition=titan
 #SBATCH --gres=gpu:1          #--gres=gpu:2 for two GPU, etc
 #SBATCH --output=job_results/model_with_dropout2d_layers_prob_0_2.o%j   #Path and name to the file for the STDOUT
 #SBATCH --error=job_results/model_with_dropout2d_layers_prob_0_2.e%j    #Path and name to the file for the STDERR
