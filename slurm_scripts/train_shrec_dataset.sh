e5aab03a6ef8e6cf01cca061b60a3bf3850e2ae1
Marko Janic <marko.janic@hotmail.ch>
2025-06-12 00:19:56 +0200

    Change partition

--- slurm_scripts/train_shrec_dataset.sh
+++ slurm_scripts/train_shrec_dataset.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=bigger_vit_no_finetune     #Name of your job
 #SBATCH --time=1-00:00:00    #Maximum allocated time
 #SBATCH --qos=gpu1day      #Selected queue to allocate your job (the gpu before 6hours is important, see comment: https://wiki.biozentrum.unibas.ch/display/scicore/9.+Requesting+GPUs)
-#SBATCH --partition=a100-80g
+#SBATCH --partition=titan
 #SBATCH --gres=gpu:1          #--gres=gpu:2 for two GPU, etc
 #SBATCH --output=job_results/bigger_vit_no_finetune.o%j   #Path and name to the file for the STDOUT
 #SBATCH --error=job_results/bigger_vit_no_finetune.e%j    #Path and name to the file for the STDERR
