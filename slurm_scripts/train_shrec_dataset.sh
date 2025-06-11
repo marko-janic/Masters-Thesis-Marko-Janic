a1625a397325c1b6d661c4333c3a7a94df7df4f5
Marko Janic <marko.janic@hotmail.ch>
2025-06-12 00:35:15 +0200

    Adjust partition

--- slurm_scripts/train_shrec_dataset.sh
+++ slurm_scripts/train_shrec_dataset.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=different_particle     #Name of your job
 #SBATCH --time=1-00:00:00    #Maximum allocated time
 #SBATCH --qos=gpu1day      #Selected queue to allocate your job (the gpu before 6hours is important, see comment: https://wiki.biozentrum.unibas.ch/display/scicore/9.+Requesting+GPUs)
-#SBATCH --partition=titan
+#SBATCH --partition=rtx4090
 #SBATCH --gres=gpu:1          #--gres=gpu:2 for two GPU, etc
 #SBATCH --output=job_results/different_particle.o%j   #Path and name to the file for the STDOUT
 #SBATCH --error=job_results/different_particle.e%j    #Path and name to the file for the STDERR
