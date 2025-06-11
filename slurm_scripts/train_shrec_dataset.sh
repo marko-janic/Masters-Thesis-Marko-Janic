5f385d2981b7fd6b6337039d257ff1be3f86dcfa
Marko Janic <marko.janic@hotmail.ch>
2025-06-11 22:04:37 +0200

    Prepare for experiment

--- slurm_scripts/train_shrec_dataset.sh
+++ slurm_scripts/train_shrec_dataset.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #The previous line is mandatory
 
-#SBATCH --job-name=slightly_smaller_z_depth_finetuned_vit_dropout_prob_0.5     #Name of your job
+#SBATCH --job-name=much_smaller_model_no_finetuning     #Name of your job
 #SBATCH --time=1-00:00:00    #Maximum allocated time
 #SBATCH --qos=gpu1day      #Selected queue to allocate your job (the gpu before 6hours is important, see comment: https://wiki.biozentrum.unibas.ch/display/scicore/9.+Requesting+GPUs)
 #SBATCH --partition=a100-80g
 #SBATCH --gres=gpu:1          #--gres=gpu:2 for two GPU, etc
-#SBATCH --output=job_results/slightly_smaller_z_depth_finetuned_vit_dropout_prob_0.5.o%j   #Path and name to the file for the STDOUT
-#SBATCH --error=job_results/slightly_smaller_z_depth_finetuned_vit_dropout_prob_0.5.e%j    #Path and name to the file for the STDERR
+#SBATCH --output=job_results/much_smaller_model_no_finetuning.o%j   #Path and name to the file for the STDOUT
+#SBATCH --error=job_results/much_smaller_model_no_finetuning.e%j    #Path and name to the file for the STDERR
 #SBATCH --cpus-per-task=8     #Number of cores to reserve
 #SBATCH --mem-per-cpu=8G     #Amount of RAM/core to reserve
 
