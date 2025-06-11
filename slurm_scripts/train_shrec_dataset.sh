833ae106601489e10c543588b5826fa964c3af81
Marko Janic <marko.janic@hotmail.ch>
2025-06-12 00:22:06 +0200

    Prepare for finetune experiment

--- slurm_scripts/train_shrec_dataset.sh
+++ slurm_scripts/train_shrec_dataset.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #The previous line is mandatory
 
-#SBATCH --job-name=bigger_vit_no_finetune     #Name of your job
+#SBATCH --job-name=finetune_last_2_vit_blocks     #Name of your job
 #SBATCH --time=1-00:00:00    #Maximum allocated time
 #SBATCH --qos=gpu1day      #Selected queue to allocate your job (the gpu before 6hours is important, see comment: https://wiki.biozentrum.unibas.ch/display/scicore/9.+Requesting+GPUs)
 #SBATCH --partition=titan
 #SBATCH --gres=gpu:1          #--gres=gpu:2 for two GPU, etc
-#SBATCH --output=job_results/bigger_vit_no_finetune.o%j   #Path and name to the file for the STDOUT
-#SBATCH --error=job_results/bigger_vit_no_finetune.e%j    #Path and name to the file for the STDERR
+#SBATCH --output=job_results/finetune_last_2_vit_blocks.o%j   #Path and name to the file for the STDOUT
+#SBATCH --error=job_results/finetune_last_2_vit_blocks.e%j    #Path and name to the file for the STDERR
 #SBATCH --cpus-per-task=8     #Number of cores to reserve
 #SBATCH --mem-per-cpu=8G     #Amount of RAM/core to reserve
 
