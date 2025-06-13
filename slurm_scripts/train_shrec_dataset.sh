e3e6af12fc3a1e20e5fb3f241717971629683bc0
Marko Janic <marko.janic@hotmail.ch>
2025-06-13 15:08:50 +0200

    Prepare for vit not in eval experiment

--- slurm_scripts/train_shrec_dataset.sh
+++ slurm_scripts/train_shrec_dataset.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #The previous line is mandatory
 
-#SBATCH --job-name=testing_different_particle_sizes_and_random_sub_micrographs_again     #Name of your job
+#SBATCH --job-name=testing_different_particle_sizes_and_random_sub_micrographs_again_VIT_NOT_IN_EVAL     #Name of your job
 #SBATCH --time=1-00:00:00    #Maximum allocated time
 #SBATCH --qos=gpu1day      #Selected queue to allocate your job (the gpu before 6hours is important, see comment: https://wiki.biozentrum.unibas.ch/display/scicore/9.+Requesting+GPUs)
 #SBATCH --partition=titan
 #SBATCH --gres=gpu:1          #--gres=gpu:2 for two GPU, etc
-#SBATCH --output=job_results/testing_different_particle_sizes_and_random_sub_micrographs_again.o%j   #Path and name to the file for the STDOUT
-#SBATCH --error=job_results/testing_different_particle_sizes_and_random_sub_micrographs_again.e%j    #Path and name to the file for the STDERR
+#SBATCH --output=job_results/testing_different_particle_sizes_and_random_sub_micrographs_again_VIT_NOT_IN_EVAL.o%j   #Path and name to the file for the STDOUT
+#SBATCH --error=job_results/testing_different_particle_sizes_and_random_sub_micrographs_again_VIT_NOT_IN_EVAL.e%j    #Path and name to the file for the STDERR
 #SBATCH --cpus-per-task=8     #Number of cores to reserve
 #SBATCH --mem-per-cpu=8G     #Amount of RAM/core to reserve
 
