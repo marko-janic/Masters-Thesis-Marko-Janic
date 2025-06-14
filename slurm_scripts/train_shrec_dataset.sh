b824492414896c062d882d832148ba437150c09f
Marko Janic <marko.janic@hotmail.ch>
2025-06-15 01:55:20 +0200

    Prepare for best fbp xperiment

--- slurm_scripts/train_shrec_dataset.sh
+++ slurm_scripts/train_shrec_dataset.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #The previous line is mandatory
 
-#SBATCH --job-name=potential_best_model    #Name of your job
+#SBATCH --job-name=potential_best_model_fbp_no_noise    #Name of your job
 #SBATCH --time=7-00:00:00    #Maximum allocated time
 #SBATCH --qos=gpu1week      #Selected queue to allocate your job (the gpu before 6hours is important, see comment: https://wiki.biozentrum.unibas.ch/display/scicore/9.+Requesting+GPUs)
 #SBATCH --partition=titan
 #SBATCH --gres=gpu:1          #--gres=gpu:2 for two GPU, etc
-#SBATCH --output=job_results/potential_best_model.o%j   #Path and name to the file for the STDOUT
-#SBATCH --error=job_results/potential_best_model.e%j    #Path and name to the file for the STDERR
+#SBATCH --output=job_results/potential_best_model_fbp_no_noise.o%j   #Path and name to the file for the STDOUT
+#SBATCH --error=job_results/potential_best_model_fbp_no_noise.e%j    #Path and name to the file for the STDERR
 #SBATCH --cpus-per-task=16     #Number of cores to reserve
 #SBATCH --mem-per-cpu=8G     #Amount of RAM/core to reserve
 
