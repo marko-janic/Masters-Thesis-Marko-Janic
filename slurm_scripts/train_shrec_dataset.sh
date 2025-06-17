594adddaa0a37c5e05af0097e7435428ff631a35
Marko Janic <marko.janic@hotmail.ch>
2025-06-17 19:55:31 +0200

    Prepare for 4 model xperiment

--- slurm_scripts/train_shrec_dataset.sh
+++ slurm_scripts/train_shrec_dataset.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #The previous line is mandatory
 
-#SBATCH --job-name=training_with_shrecs_reconstruction_volume_bigger_model    #Name of your job
+#SBATCH --job-name=fbp_20dB_4_training_volumes    #Name of your job
 #SBATCH --time=7-00:00:00    #Maximum allocated time
 #SBATCH --qos=gpu1week      #Selected queue to allocate your job (the gpu before 6hours is important, see comment: https://wiki.biozentrum.unibas.ch/display/scicore/9.+Requesting+GPUs)
 #SBATCH --partition=a100-80g
 #SBATCH --gres=gpu:1          #--gres=gpu:2 for two GPU, etc
-#SBATCH --output=job_results/training_with_shrecs_reconstruction_volume_bigger_model.o%j   #Path and name to the file for the STDOUT
-#SBATCH --error=job_results/training_with_shrecs_reconstruction_volume_bigger_model.e%j    #Path and name to the file for the STDERR
+#SBATCH --output=job_results/fbp_20dB_4_training_volumes.o%j   #Path and name to the file for the STDOUT
+#SBATCH --error=job_results/fbp_20dB_4_training_volumes.e%j    #Path and name to the file for the STDERR
 #SBATCH --cpus-per-task=16     #Number of cores to reserve
 #SBATCH --mem-per-cpu=8G     #Amount of RAM/core to reserve
 
