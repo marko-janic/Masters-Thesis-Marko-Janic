f314c17a20aad83d55fded92c19ddaef9ef31fc5
Marko Janic <marko.janic@hotmail.ch>
2025-06-19 16:23:55 +0200

    Prepare for 10 finetunting layers ablation study

--- slurm_scripts/train_shrec_dataset.sh
+++ slurm_scripts/train_shrec_dataset.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #The previous line is mandatory
 
-#SBATCH --job-name=shrecs_reconstruction_volume_attempt_2    #Name of your job
+#SBATCH --job-name=finetune_10_vit_layers    #Name of your job
 #SBATCH --time=7-00:00:00    #Maximum allocated time
 #SBATCH --qos=gpu1week      #Selected queue to allocate your job (the gpu before 6hours is important, see comment: https://wiki.biozentrum.unibas.ch/display/scicore/9.+Requesting+GPUs)
 #SBATCH --partition=rtx4090
 #SBATCH --gres=gpu:1          #--gres=gpu:2 for two GPU, etc
-#SBATCH --output=job_results/shrecs_reconstruction_volume_attempt_2.o%j   #Path and name to the file for the STDOUT
-#SBATCH --error=job_results/shrecs_reconstruction_volume_attempt_2.e%j    #Path and name to the file for the STDERR
+#SBATCH --output=job_results/finetune_10_vit_layers.o%j   #Path and name to the file for the STDOUT
+#SBATCH --error=job_results/finetune_10_vit_layers.e%j    #Path and name to the file for the STDERR
 #SBATCH --cpus-per-task=16     #Number of cores to reserve
 #SBATCH --mem-per-cpu=8G     #Amount of RAM/core to reserve
 
