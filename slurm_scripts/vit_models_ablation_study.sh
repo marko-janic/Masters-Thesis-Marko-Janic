5c72e87e8c351bf16d80162effcf5308b1d56bbf
Marko Janic <marko.janic@hotmail.ch>
2025-06-23 03:34:34 +0200

    Adjust partitions for ablation studies

--- slurm_scripts/vit_models_ablation_study.sh
+++ slurm_scripts/vit_models_ablation_study.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=train_different_vit_models    #Name of your job
 #SBATCH --time=7-00:00:00    #Maximum allocated time
 #SBATCH --qos=gpu1week      #Selected queue to allocate your job (the gpu before 6hours is important, see comment: https://wiki.biozentrum.unibas.ch/display/scicore/9.+Requesting+GPUs)
-#SBATCH --partition=a100
+#SBATCH --partition=titan
 #SBATCH --gres=gpu:1          #--gres=gpu:2 for two GPU, etc
 #SBATCH --output=job_results/train_different_vit_models.o%j   #Path and name to the file for the STDOUT
 #SBATCH --error=job_results/train_different_vit_models.e%j    #Path and name to the file for the STDERR
