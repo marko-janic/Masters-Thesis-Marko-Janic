71e65dd847ee3435fd285c82a96061a717ece953
Marko Janic <marko.janic@hotmail.ch>
2025-06-24 00:59:18 +0200

    shrec xperiment again

--- slurm_scripts/train_shrec_dataset.sh
+++ slurm_scripts/train_shrec_dataset.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #The previous line is mandatory
 
-#SBATCH --job-name=base_model_-5dB    #Name of your job
+#SBATCH --job-name=base_model_shrec_reconstruction    #Name of your job
 #SBATCH --time=7-00:00:00    #Maximum allocated time
 #SBATCH --qos=gpu1week      #Selected queue to allocate your job (the gpu before 6hours is important, see comment: https://wiki.biozentrum.unibas.ch/display/scicore/9.+Requesting+GPUs)
 #SBATCH --partition=rtx4090
 #SBATCH --gres=gpu:1          #--gres=gpu:2 for two GPU, etc
-#SBATCH --output=job_results/base_model_-5dB.o%j   #Path and name to the file for the STDOUT
-#SBATCH --error=job_results/base_model_-5dB.e%j    #Path and name to the file for the STDERR
+#SBATCH --output=job_results/base_model_shrec_reconstruction.o%j   #Path and name to the file for the STDOUT
+#SBATCH --error=job_results/base_model_shrec_reconstruction.e%j    #Path and name to the file for the STDERR
 #SBATCH --cpus-per-task=16     #Number of cores to reserve
 #SBATCH --mem-per-cpu=8G     #Amount of RAM/core to reserve
 
