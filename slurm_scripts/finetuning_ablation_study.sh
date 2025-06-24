4b1323aa3bc235be5ff1e46072170072ca4c36bd
Marko Janic <marko.janic@hotmail.ch>
2025-06-24 13:33:36 +0200

    Update finetuning_ablation_study.sh

--- slurm_scripts/finetuning_ablation_study.sh
+++ slurm_scripts/finetuning_ablation_study.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=finetuning_ablation_study_from_4_layers_up    #Name of your job
 #SBATCH --time=7-00:00:00    #Maximum allocated time
 #SBATCH --qos=gpu1week      #Selected queue to allocate your job (the gpu before 6hours is important, see comment: https://wiki.biozentrum.unibas.ch/display/scicore/9.+Requesting+GPUs)
-#SBATCH --partition=rtx4090
+#SBATCH --partition=a100-80g
 #SBATCH --gres=gpu:1          #--gres=gpu:2 for two GPU, etc
 #SBATCH --output=job_results/finetuning_ablation_study_from_4_layers_up.o%j   #Path and name to the file for the STDOUT
 #SBATCH --error=job_results/finetuning_ablation_study_from_4_layers_up.e%j    #Path and name to the file for the STDERR
