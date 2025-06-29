dfcceed4eadf6bc66446c2e79d13b788dd692650
Marko Janic <marko.janic@hotmail.ch>
2025-06-29 21:54:41 +0200

    Update train_shrec_dataset.sh

--- slurm_scripts/train_shrec_dataset.sh
+++ slurm_scripts/train_shrec_dataset.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=best_model_shrec_reconstruction_base_7_train    #Name of your job
 #SBATCH --time=7-00:00:00    #Maximum allocated time
 #SBATCH --qos=gpu1week      #Selected queue to allocate your job (the gpu before 6hours is important, see comment: https://wiki.biozentrum.unibas.ch/display/scicore/9.+Requesting+GPUs)
-#SBATCH --partition=a100-80g
+#SBATCH --partition=rtx4090
 #SBATCH --gres=gpu:1          #--gres=gpu:2 for two GPU, etc
 #SBATCH --output=job_results/best_model_shrec_reconstruction_base_7_train.o%j   #Path and name to the file for the STDOUT
 #SBATCH --error=job_results/best_model_shrec_reconstruction_base_7_train.e%j    #Path and name to the file for the STDERR
